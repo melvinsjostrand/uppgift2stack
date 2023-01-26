b6c31cdb56b7ee5544619676af2785ea51676031
melvinsjostrand <mesj04001@utb.vaxjo.se>
2023-01-26 12:23:07 +0100

    :D

--- upgift2/Program.cs
+++ upgift2/Program.cs
@@ -1,52 +1,59 @@
-﻿using System;
-using System.Collections;
-
-class uppgift2{
-
-    static bool isOperator(char x){
-
-switch(x)
-        {
-            case '+':
-            case '^':
-            case '%':
-            case '-':
-            case '/':
-            case '*':
-            return true;
-        }
-        return false;
-    }
-public static string convert(string str)
-{
-    Stack stack = new Stack();
-
-    int l = str.Length;
-
-    for(int i = l - 1; i >= 0; i--)
+﻿class Program
     {
-        char c = str[i];
-        
-        if (isOperator(c))
+        static void Main(string[] args)
         {
-            string op1 = (string)stack.Pop();
-            string op2 = (string)stack.Pop();
+            Stack<int> stack = new Stack<int>();
+            //the expression will be a string array
+            string[] expression = {"*" , "+" , "1" , "2" , "-" , "3" , "4"};
+            for (int i = 0; i < expression.Length; i++)//we write the prefix expression
+            {
+                Console.Write(expression[i]);
+            }
+            int result;//a result to push onto the stack after an operation was done
 
-            string temp = "(" +op1 + c +op2 + ")";
-            stack.Push(temp);
-        }
-        else
-        {
-        stack.Push(c + "");
+            Console.WriteLine();//newline space
+            Array.Reverse(expression);//we reverse the expression to read in the characters from right to left
+            int n;
+            foreach (string c in expression)//for each string character in the array
+            {
+                if (int.TryParse(c, out n))//if the character can be converted to a number (operand)
+                {
+                    stack.Push(n);//push the number onto the stack
+                }
+                if (c == "+")//handling of operators
+                {
+                    int x = stack.Pop();
+                    int y = stack.Pop();
+                    result = x + y;//evaluate the values popped from the stack
+                    stack.Push(result);//push current result onto the stack
+                }
+                if (c == "-")
+                {
+                    int x = stack.Pop();
+                    int y = stack.Pop();
+                    result = x - y;
+                    stack.Push(result);
+                }
+                if (c == "*")
+                {
+                    int x = stack.Pop();
+                    int y = stack.Pop();
+                    result = x * y;
+                    stack.Push(result);
+                }
+                if (c == "/")
+                {
+                    int x = stack.Pop();
+                    int y = stack.Pop();
+                    result = y / x;
+                    stack.Push(result);
+                }
+
+            }
+            /*write the final result of the expression,
+             * which is at the top of the stack, so we use Peek()*/
+            Console.WriteLine("result of expression: {0}", stack.Peek());
+
+            Console.ReadLine();
         }
     }
-    return (string)stack.Pop();
-}
-public static void Main(string[] args)
-{
-    string input ="*+12-34";
-
-    string convertedinput = convert(input);
-    Console.WriteLine(convertedinput);
-}
-}
